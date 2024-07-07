35126f9cc42a6de3e0c562a59dd4046aaa2fa4c0
ravi1280 <ravishkaindrajith9.9@gmail.com>
2024-07-08 00:22:50 +0530

    Arrow function file created

---
+++ arow_Function.js
@@ -0,0 +1,6 @@
+// Mainly  arrow function can't see *Function* Key Word
+const welcome=(name)=>{
+    return 'Hello '+name+'! . This is arrow Function'
+}
+
+console.log(welcome('User_Name'));
---
+++ basic_Function.js
@@ -0,0 +1,10 @@
+function welcome(name) {
+
+    return "Hello " + name + "!";
+    
+  }
+
+  console.log(welcome("User_Name"));
+  
+  //    call welcome function and print return value of welcome function using *console.log*
+  
