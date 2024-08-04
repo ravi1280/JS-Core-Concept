b950a52110bf2f85612671124dc9f2d976409567
ravi1280 <ravishkaindrajith9.9@gmail.com>
2024-08-04 19:27:09 +0530

    create arrow function 2 file

--- basic_Function.js
+++ basic_Function.js
@@ -1,9 +1,6 @@
 function welcome(name) {
-
     return "Hello " + name + "!";
-    
   }
-
   console.log(welcome("User_Name"));
   
   //    call welcome function and print return value of welcome function using *console.log*
