c3e4969d59dccbe0c92487af5471b7fe1e048e3b
ravi1280 <ravishkaindrajith9.9@gmail.com>
2024-08-04 19:42:30 +0530

    new update

--- arrow_Function_2.js
+++ arrow_Function_2.js
@@ -1,1 +1,17 @@
-hh
+//first action
+function add(a,b){
+    return a+b;
+}
+console.log(add(6,4));
+
+
+//second action
+const add2 =(a,b)=>{
+    return a+b;
+}
+console.log(add2(10,4));
+
+
+//third action 
+const add3=(a,b)=>a+b;
+console.log(add3(10,10));
